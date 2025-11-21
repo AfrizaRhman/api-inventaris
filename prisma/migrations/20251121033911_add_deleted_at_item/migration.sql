efa5cf7dd1ce627c42450c0a9c26f69f868c7969
M Afriza Rahman <muhamadafrizarahman@smkwikrama.sch.id>
2025-11-21 11:36:26 +0700

    cebok

---
+++ prisma/migrations/20251121033911_add_deleted_at_item/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `created_at` to the `Item` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `Item` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Item" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
